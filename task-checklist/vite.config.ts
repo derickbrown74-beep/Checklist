ff5eb5e8856a5a25eb956b8a3bf3a3a2d06a06ab
kine <derick.brown74@yahoo.com>
2025-09-21 14:53:13 -0500

    Update deployment configuration for GitHub Pages

--- task-checklist/vite.config.ts
+++ task-checklist/vite.config.ts
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Checklist/', // Repository name
+  base: './',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets'
+    assetsDir: 'assets',
+    sourcemap: true
   }
 })
